43af14362701ef810dd9b176b4caf10d711a94bb
Raul Palacios <palacios.raul02@gmail.com>
2021-07-21 10:46:32 -0500

    Remove unnecesary test

--- test/prescrypto_test.rb
+++ test/prescrypto_test.rb
@@ -6,8 +6,4 @@ class PrescryptoTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Prescrypto::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
